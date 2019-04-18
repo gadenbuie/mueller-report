ab74012b0532ffa34f3a45196d2b28004e11b9c2
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2019-04-18 12:14:16 -0400

    Intial ████████

---
+++ process_report.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(pdftools)
+
+# Download report from https://www.documentcloud.org/documents/5955210-Redacted-Mueller-Report.html
+mueller_report_txt <- pdf_text("Redacted-Mueller-Report.pdf")
+
+mueller_report <- tibble(
+  page = 1:length(mueller_report_txt),
+  text = mueller_report_txt
+) %>% 
+  separate_rows(text, sep = "\n") %>% 
+  group_by(page) %>% 
+  mutate(line = row_number()) %>% 
+  ungroup() %>% 
+  select(page, line, text)
+
+write_csv(mueller_report, "mueller_report.csv")
